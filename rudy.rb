798e7c16c1b4384281c36e7e46ee0fd35eaf67cd
ankithooda <ankit.hooda@joshtechnologygroup.com>
2015-07-25 23:39:14 +0530

    Fixed typos, checked for correct encoding of input to notify-send

--- rudy.rb
+++ rudy.rb
@@ -1,7 +1,8 @@
 require 'json'
 require 'net/http'
 
-REDDIT_URL = 'www.reddit.com'
+TODO: Get this constants from a config file
+REDDIT_URL = 'http://www.reddit.com'
 HOT = 'hot'
 NEW = 'new'
 NOTIFY_TIME = 10
@@ -11,23 +12,45 @@ NOTIFY_TIME = 10
 # subreddit['msg']
 def fetch_subreddit_latest_post subreddit, post_type
   begin
+
     puts "#{Time.now} : Fetching reddit posts for #{subreddit['name']}"
+
     post_list = []
     subreddit_url = "#{REDDIT_URL}/r/#{subreddit['name']}/#{post_type}.json?sort=new"
     result = Net::HTTP.get(URI.parse(subreddit_url))
     json_data = JSON.parse(result)
-    json["data"]["children"].each do |post|
-      post.push post["data"]["title"]
+    json_data["data"]["children"].each do |post|
+      post_list.push post["data"]["title"]
     end
+
     puts "#{Time.now} : Fetched #{post_list.length} posts"
-    `notify-send -t #{NOTIFY_TIME} #{subreddit['msg']} '#{post_list.join("\n")}'`
-  rescue StandardException => e
+
+    # Notify in groups of 5 posts, since notify-send uses a daemon
+    # all the notify-send calls will queued
+    post_list.each_slice(5).each do |posts|
+      # Sanitize the message string
+      notification_title = subreddit['msg']
+      notification_message = posts.join('\n')
+
+      # If it not a valid encoding then convert it into one
+      # see here - http://stackoverflow.com/questions/29877310/invalid-byte-sequence-in-utf-8-argumenterror
+      # then gsub it
+      if !notification_message.valid_encoding?
+        notification_message = notification_message.encode("UTF-16be", :invalid=>:replace, :replace=>"").encode('UTF-8')
+      end
+      notification_message.gsub!("'", "")
+
+      `notify-send -t #{NOTIFY_TIME} '#{notification_title}' '#{notification_message}'`
+    end
+
+  rescue StandardError => e
     puts "#{e}"
     puts "#{e.backtrace.join("\n")}"
   end
 end
 
 def main
+  TODO: Get this hash from a config file
   subreddit_list = [
     {'name' => 'programming',
       'msg' => 'Hot on /programming'
@@ -39,7 +62,7 @@ def main
   ]
 
   subreddit_list.each do |subreddit|
-    fetch_subreddit_last_post subreddit
+    fetch_subreddit_latest_post subreddit, HOT
 
     # Sleep for half minute before fetching next
     sleep NOTIFY_TIME + 30
